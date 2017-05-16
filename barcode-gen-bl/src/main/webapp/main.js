b1359fa4dd44d87f0c621032a894b1406600c776
Csaba <Csaba@ollepc.t.hu>
2017-05-16 10:34:23 +0200

    commit

---
+++ barcode-gen-bl/src/main/webapp/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    $("#getPrice").submit(function (event) {
+
+        //stop submit the form, we will post it manually.
+        event.preventDefault();
+
+        fire_ajax_submit();
+
+    });
+
+});
+
+function fire_ajax_submit() {
+	
+	<!--
+    var search = {}
+    search["userCode"] = $("#userCode").val();
+	  search["priceType"] = $("#priceType").val();
+
+    $("#getPrice").prop("disabled", true);
+	-->
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "http://localhost:8080/barcode-gen-bl/barcode/getPrice",
+		headers: { 'Access-Control-Allow-Origin': '*' },
+        data: JSON.stringify({
+                    userCode: "sa",
+                    priceType: 1
+                }),
+        dataType: 'json',
+        cache: false,
+        timeout: 600000,
+        success: function (data) {
+
+            var json = "<h4>Ajax Response</h4><pre>"
+                + JSON.stringify(data, null, 4) + "</pre>";
+            $('#feedback').html(json);
+
+            console.log("SUCCESS : ", data);
+            $("#btn-search").prop("disabled", false);
+
+        },
+        error: function (e) {
+
+            var json = "<h4>Ajax Response</h4><pre>"
+                + e.responseText + "</pre>";
+            $('#feedback').html(json);
+
+            console.log("ERROR : ", e);
+            $("#btn-search").prop("disabled", false);
+
+        }
+    });
+
+}
